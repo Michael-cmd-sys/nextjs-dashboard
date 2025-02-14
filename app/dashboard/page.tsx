94b1fd3c7a40bcfeae730bb1267871177245c48d
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-02-14 10:26:29 +0000

    basic dashboard project structure

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+      <h1 className="text-6xl font-bold">Dashboard</h1>
+    </div>
+  );
+}
